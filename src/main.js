294815ab7d6c072ffbe3f70ad440ec459eba7bb9
Swallow <1299228270@qq.com>
2018-05-07 17:41:32 +0800

    添加elementUi 引用

--- src/main.js
+++ src/main.js
@@ -3,14 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import loading from './plugin/loading'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 import './assets/scss/index.css';
 
+Vue.use(ElementUI);
+Vue.use(loading)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
