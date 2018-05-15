b61ebdbe2330afef2650892c248fc7ad29e06dba
Swallow <1299228270@qq.com>
2018-05-15 17:24:42 +0800

    添加滚动 表单验证的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(Router);
 
 
 
 const index = r => require.ensure([], () => r(require('../view/index')), 'index');
 const transformExp = r => require.ensure([], () => r(require('../view/transformExp')), 'transformExp');
-
+const scrollbar = r => require.ensure([], () => r(require('../view/scrollbar')), 'scrollbar');
+const formValidator = r => require.ensure([], () => r(require('../view/formValidator')), 'formValidator');
 export default new Router({
   routes: [{
       path: '/',
@@ -18,6 +19,16 @@ export default new Router({
       path: '/transformExp',
       name: 'transformExp',
       component: transformExp
+    },
+    {
+      path: '/scrollbar',
+      name: 'scrollbar',
+      component: scrollbar
+    },
+    {
+      path: '/formValidator',
+      name: 'formValidator',
+      component: formValidator
     }
   ]
 })
