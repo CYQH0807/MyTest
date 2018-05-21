32f91e0252412fe223f7248a2b4d09373375f7d8
Swallow <1299228270@qq.com>
2018-05-21 11:26:58 +0800

    添加滚动条插件的引用

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 import './assets/scss/index.css';
+import betterScroll from './components/betterScroll.vue'
 
 Vue.use(ElementUI);
 Vue.use(loading)
+Vue.component('better-scroll', betterScroll)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
