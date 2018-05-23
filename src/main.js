d9544cba3ba804b3deec0eb078cb104b59f9acf7
Swallow <1299228270@qq.com>
2018-05-23 13:14:18 +0800

    添加error组件

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import loading from './plugin/loading'
+import error from './plugin/error'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-import './assets/scss/index.css';
+import './assets/scss/index.scss';
 import betterScroll from './components/betterScroll.vue'
 
 Vue.use(ElementUI);
-Vue.use(loading)
+Vue.use(loading);
+Vue.use(error);
 Vue.component('better-scroll', betterScroll)
 /* eslint-disable no-new */
 new Vue({
