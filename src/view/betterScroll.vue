0172e6be4a6c4c76b5f1522c29b45c10e6ad5947
Swallow <1299228270@qq.com>
2018-05-23 15:45:58 +0800

    ...

--- src/view/betterScroll.vue
+++ src/view/betterScroll.vue
@@ -1,38 +1,49 @@
 <template>
-    <better-scroll :data="movieList" class="wrapper" ref="wrapper1">
-        <el-table :data="movieList" style="width: 100%">
-            <el-table-column prop="id" label="ID" width="100">
-            </el-table-column>
-            <el-table-column label="中文名称" width="180">
-                <template slot-scope="scope">
-                    <a :href="scope.row.alt" target="_blank" class="movieTableLink">{{ scope.row.title }}</a>
-                </template>
-            </el-table-column>
-            <el-table-column class="cursorPointer" label="英文名称">
-                <template slot-scope="scope">
-                    <span class="cursorPointer"> {{ scope.row.original_title }} </span>
-                </template>
-            </el-table-column>
-            <el-table-column prop="year" label="年份" width="100">
-            </el-table-column>
-            <el-table-column label="评分" width="200">
-                <template slot-scope="scope">
-                    <el-rate v-model="scope.row.rating.average" disabled show-score text-color="#ff9900" score-template="{value}">
-                    </el-rate>
-                </template>
-            </el-table-column>
+    <div>
+        <el-form :inline="true" :model="form" class="demo-form-inline">
+            <el-form-item label="url参数">
+                <el-input v-model="form.url"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">url参数追加</el-button>
+                <el-button type="primary" @click="normalQuery">正常查询</el-button>
+                <el-button type="primary" @click="expiredQuery">过期查询</el-button>
+            </el-form-item>
+        </el-form>
+        <better-scroll :data="movieList" class="wrapper" ref="wrapper1">
+            <el-table :data="movieList" style="width: 100%">
+                <el-table-column prop="id" label="ID" width="100">
+                </el-table-column>
+                <el-table-column label="中文名称" width="180">
+                    <template slot-scope="scope">
+                        <a :href="scope.row.alt" target="_blank" class="movieTableLink">{{ scope.row.title }}</a>
+                    </template>
+                </el-table-column>
+                <el-table-column class="cursorPointer" label="英文名称">
+                    <template slot-scope="scope">
+                        <span class="cursorPointer"> {{ scope.row.original_title }} </span>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="year" label="年份" width="100">
+                </el-table-column>
+                <el-table-column label="评分" width="200">
+                    <template slot-scope="scope">
+                        <el-rate v-model="scope.row.rating.average" disabled show-score text-color="#ff9900" score-template="{value}">
+                        </el-rate>
+                    </template>
+                </el-table-column>
 
-            <el-table-column label="分类">
-                <template slot-scope="scope">
-                    <el-tag type="success" class="marginRight7" v-for="(item,index) in scope.row.genres" :key="index">{{item}}</el-tag>
-                </template>
-            </el-table-column>
+                <el-table-column label="分类">
+                    <template slot-scope="scope">
+                        <el-tag type="success" class="marginRight7" v-for="(item,index) in scope.row.genres" :key="index">{{item}}</el-tag>
+                    </template>
+                </el-table-column>
 
-        </el-table>
+            </el-table>
 
-        <div class="aaa" @click="scrollPosition">123123</div>
-
-    </better-scroll>
+            <div class="aaa" @click="scrollPosition">123123</div>
+        </better-scroll>
+    </div>
 </template>
 
 <script>
@@ -48,7 +59,10 @@ export default {
   data() {
     return {
       movieList: [],
-      scroll: undefined
+      scroll: undefined,
+      form: {
+        url: ""
+      }
     };
   },
 
@@ -64,15 +78,52 @@ export default {
         that.movieList = data.subjects;
         that.total = data.total;
       });
+    let query = this.$router.currentRoute.query;
+    if (query) {
+      Object.keys(query).forEach(key => {
+        this.form.url += key + "=" + query[key] + "&&";
+      });
+    }
+    this.form.url = this.form.url.substr(0, this.form.url.length - 2);
   },
 
   methods: {
+    loadData() {},
     scrollPosition() {
       //父组件获取子组件的vm实例 使用this.$refs
       //当refs这个属性在组件上时  this.$refs 获取的是改组件的VM实例 即可以调用子组件的方法
       //当refs属性在DOM元素上时  this.$refs获取的就是当前的DOM元素
       let scroll = this.$refs.wrapper1;
       scroll.scrollTo(0, -200);
+    },
+    onSubmit() {
+      this.$router.replace(
+        this.$router.currentRoute.path + "?" + this.form.url
+      );
+    },
+    normalQuery() {
+      let that = this;
+      axios
+        .JsonpTest("", { count: 50, start: 0 }, { container: ".wrapper" })
+        .then(data => {
+          that.movieList = data.subjects;
+          that.total = data.total;
+        });
+    },
+    expiredQuery() {
+      let that = this;
+      axios
+        .JsonpTest(
+          "",
+          { count: 50, start: 0 },
+          { container: ".wrapper" },
+          true,
+          true
+        )
+        .then(data => {
+          that.movieList = data.subjects;
+          that.total = data.total;
+        });
     }
   }
 };
