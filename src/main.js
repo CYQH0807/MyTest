ca8684b5b85593656531470bd471faaae5e39ccb
Swallow <1299228270@qq.com>
2018-05-28 15:42:08 +0800

    字体以及background 路径测试引用

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-require("./assets/css/test.css")
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import loading from './plugin/loading'
 import error from './plugin/error'
 import ElementUI from 'element-ui';
+require("./assets/scss/index.scss");
+import "./assets/css/test.css"
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 import betterScroll from './components/betterScroll.vue'
